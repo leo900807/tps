fe1faa70216f1db2e5bcdf87819bd9315cf37fe3
baluteshih <brian369999@gmail.com>
2021-08-16 20:36:59 +0800

    Update pgg.py

--- template/scripts/internal/pgg.py
+++ template/scripts/internal/pgg.py
@@ -34,28 +34,37 @@ def canonical(s):
     return s.strip().lower().replace(' ', '').replace('-', '').replace('_', '')
 
 
-def similar(string, pattern):
-    return edit_distance(canonical(string), canonical(pattern)) <= 2
+def similar(string, pattern_set):
+    for pattern in pattern_set:
+        if edit_distance(canonical(string), canonical(pattern)) <= 2:
+            return True
+    return False
 
 
-BEGIN_SECRET = '// BEGIN SECRET'
-END_SECRET = '// END SECRET'
+BEGIN_SECRET = {'// BEGIN SECRET', '# BEGIN SECRET'}
+END_SECRET = {'// END SECRET', '# END SECRET'}
 
 printLine = True
 output = []
 for line in sys.stdin:
     lineNo += 1
-    if line.strip() == BEGIN_SECRET:
+    if line.strip() in BEGIN_SECRET:
         if not printLine:
             die('already in secret')
         printLine = False
     elif similar(line, BEGIN_SECRET):
         die('similar to "%s"' % BEGIN_SECRET)
     if printLine:
+        while line.find('_secret') != -1:
+            begin_place = line.find('_secret')
+            if line.find('secret', begin_place + 7) == -1:
+                die('_secret ... secret matching error in line "%s"' % line)
+            end_place = line.find('secret', begin_place + 7)
+            line = line[:begin_place] + line[end_place + 6:] 
         if 'secret' in line.lower():
             die('secret found in line "%s"' % line)
         output.append(line.rstrip('\n'))
-    if line.strip() == END_SECRET:
+    if line.strip() in END_SECRET:
         if printLine:
             die('not in secret')
         printLine = True
